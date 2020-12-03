485fce49d9a20d4859ca1641394070135b2a0dc2
ganoshiki <akck0130@gmail.com>
2020-12-03 11:23:26 +0900

    おみくじ完成

--- app/assets/javascripts/omikuji.js
+++ app/assets/javascripts/omikuji.js
@@ -1,9 +1,28 @@
 'use strict';
 
 {
+  // id
   const btn = document.getElementById('btn');
 
+  // 定数
+  
+  let kekka = ["大吉","中吉","小吉","凶"]
+
   btn.addEventListener('click', () => {
-    btn.textContent = 'hit!';
+    let random = Math.floor( Math.random() * 101)
+    if (random <= 10){
+      btn.textContent = kekka[0];
+    }
+    else if (random <= 50) {
+      btn.textContent = kekka[1];
+    }
+    else if (random <= 80) {
+      btn.textContent = kekka[2];
+    } 
+    else {
+      btn.textContent = kekka[3];
+    }
+   
+
   })
 }
